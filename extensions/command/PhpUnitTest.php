8fa0e48adc17c152cc176c5e5d4cfa1aeda93dc6
Buddy Lindsey <buddy.lindsey@vitals.com>
2013-02-13 20:11:53 -0600

    Move test to root and modify test runner

--- extensions/command/PhpUnitTest.php
+++ extensions/command/PhpUnitTest.php
@@ -4,19 +4,14 @@ namespace li3_phpunit\extensions\command;
 
 require_once 'PHPUnit/Autoload.php';
 
-$app_root = __DIR__."/../../../../";
-
-require_once $app_root . "tests/functional/routes/TermsOfUseRouteTest.php";
-
+use app\tests\functional\routes\TermsOfUseRouteTest;
 use \PHPUnit_Framework_TestSuite;
-use \PHPUnit_Framework_TestResult;
-
 
 class PhpUnitTest extends \lithium\console\Command {
 	public function run() {
 
 		$suite = new PHPUnit_Framework_TestSuite('TermsOfUseRouteTest');
-		
+
 		\PHPUnit_TextUI_TestRunner::run($suite);
 	}
 }
