5b188b3bd81b8e5f71090c19dca9ebfb9f62d858
Buddy Lindsey <buddy.lindsey@vitals.com>
2013-02-21 18:07:04 -0600

    oddity with controller and views

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -4,15 +4,31 @@ namespace li3_phpunit\controllers;
 
 use lithium\core\Libraries;
 use li3_phpunit\models\Results;
-
+use lithium\template\View;
 class ReportController extends \lithium\action\Controller {
 
 	public function index() {
+		error_reporting(-1);
+		echo "Hi";
+		return array("foo" => "bar");
 
-		print_r($this);die;
 		$files = Libraries::locate('phpunit', null, array('filter' => '/tests\\\.*Test/'));
 
-		return array('files' => $files);
+		$view = new View(array(
+			'paths' => array(
+				'template' => '{:library}/views/{:controller}/{:template}.{:type}.php',
+				'layout'   => '{:library}/views/layouts/{:layout}.{:type}.php',
+			)
+		));
+
+		return $view->render('all', compact('content'), compact('template') + array(
+			'data' => array('files' => $files),
+			'library' => 'li3_phpunit',
+			'controller' => 'report',
+			'layout' => 'default',
+			'template' => 'index',
+			'type' => 'html'
+		));
 	}
 
 }
